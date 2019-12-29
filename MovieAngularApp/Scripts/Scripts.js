e2f3daa1f71f8fbe2c8bb9d8fa1756d805057e05
CORPORATE\mahallem <mahesh.mahalle@act.org>
2019-12-29 09:05:14 -0600

    Add project files.

---
+++ MovieAngularApp/Scripts/Scripts.js
@@ -0,0 +1,16 @@
+﻿var app = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope, $http, $log) {
+        $http({
+            method: 'GET',
+            url: 'http://localhost:44364/movies'
+        })
+            .then(function (response) {
+                $scope.movies = response.data;
+                $log.info(response);
+            }, function (reason) {
+                $scope.error = reason.data;
+                $log.info(reason);
+            }
+            )
+    })
